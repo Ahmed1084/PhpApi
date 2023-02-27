52bbdb71bcc51428e1648aa668fb354d5c66e02f
Sufiyan1949 <56268355+Sufiyan1949@users.noreply.github.com>
2023-02-27 15:14:43 +0530

    complete

--- connect_to_mysql.php
+++ connect_to_mysql.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = 'root';
 $password = "";
-$database = 'job_portal';
+$database = 'jobportal';
 $port = 3306;
 $mysqli = new mysqli($host, $user, $password, $database, $port);mysqli_connect("localhost", "root", "") or die("Connection Failed");
 ?>
